0f7e34514c627e90c068e6fc9245e54c2d83317d
wangxiaolong <wangxiaolong@zhongan.com>
2018-08-07 11:30:17 +0800

    d

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -104,7 +104,7 @@ var prodConfig = {
         new ExtractTextPlugin('css/[name].css'),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production'),
-            'DEVELOPMETN_URL': JSON.stringify('http://localhost:8081/')
+            'DEVELOPMETN_URL': JSON.stringify('https://api.gilea.cn/')
         }),
         new HtmlWebpackPlugin({
             filename: 'index.html',
